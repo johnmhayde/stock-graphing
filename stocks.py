d10fa4cf9d8860842e2ca1026400af25f4b2e91c
John Michael Hayde <johnmichaelhayde@gmail.com>
2021-01-13 15:39:42 -0500

    adjusting date lists and how to process info into arrays

--- stocks.py
+++ stocks.py
@@ -1,7 +1,37 @@
 import yfinance as yf
+import numpy as np
+import pandas as pd
 
+# variable declaration
 stocks = ["AAPL", "GOOGL"];
+startDate = "2020-01-01";
+endDate = "2020-12-01"
 
-data = yf.download(stocks, start="2020-01-01", end="2020-12-01");
+# make list of date range
+dateList = pd.date_range(startDate, endDate);
 
-print(data.head(10));
+# initialize arrays with size of list
+numElements = len(dateList);
+x = [];
+y = np.zeros(numElements);
+# print(numElements);
+
+# store dates in x array
+for i in range(0, numElements):
+	line = str(dateList[i]).split();
+	x.append(line[0]);
+
+# pull data from yfinance
+data = yf.download("GOOGL", start=startDate, end=endDate, group_by="ticker");
+
+print(data);
+
+# for i in range(0, data[stocks[0]]['Adj Close'].size):
+# 	#line = str(element).split();
+# 	print(data[stocks[0]]['Adj Close'][i]);
+
+# # pull info from data for specific stock
+# for i in range(0, numElements):
+# 	y[i] = data[stocks[0]]['Adj Close'][i];
+
+# print(y);
